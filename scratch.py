eef0dd4cb2530d5c0f28a38a56c8785f79c9fbff
Med Mahmoud <medthehatta@gmail.com>
2024-09-28 11:16:56 -0400

    WIP

--- scratch.py
+++ scratch.py
@@ -1,6 +1,8 @@
 import os
 import random
 
+import arcade
+
 from combat import perform_combat
 from initiative import Initiative
 from unit import Unit
@@ -8,6 +10,8 @@ from unit import Module
 from unit import NormalDie
 from unit import RiftDie
 from util import short_id
+from rect import Point
+from rect import RectLRBT
 from sample import random_unit
 from sample import random_module
 from svggen import svg_for_module
@@ -58,17 +62,62 @@ def setup_combat():
     return (rng, initiative)
 
 
-def random_module_collage(num_modules, rng=None):
+def random_modules(num_modules, rng=None):
     rng = rng or random.Random()
+    return [random_module(rng) for _ in range(num_modules)]
 
-    series = short_id()
-    modules = []
 
-    for i in range(num_modules):
-        mod = random_module(rng)
-        modules.append(mod)
-        svg = svg_for_module(mod)
-        img = svg_string_to_pil(svg)
+def module_to_pil(module):
+    return svg_string_to_pil(svg_for_module(module))
+
+
+def save_module_collage(modules, series=None):
+    series = series or short_id()
+    pils = [module_to_pil(mod) for mod in modules]
+
+    for img in pils:
         img.save(os.path.join(SCRATCH_PATH, f"mod-{series}-{i}.png"))
 
-    return (modules, series)
+    return (pils, series)
+
+
+def render_module_collage(modules, num_columns=10, padding=2, margin=5):
+    if not modules:
+        raise ValueError("Must provide a nonempty list of modules")
+    pils = [module_to_pil(mod) for mod in modules]
+    pil_width = pils[0].width
+    pil_height = pils[0].height
+    basis_rect = (
+        RectLRBT.tlwh((0, 0), pil_width, pil_height)
+        .displace((margin, margin))
+    )
+
+    width = num_columns * (pil_width + padding) - padding + 2*margin
+    num_rows = len(pils) // num_columns
+    height = num_rows * (pil_height + padding) - padding + 2*margin
+
+    arcade.open_window(width, height, "Module collage")
+    arcade.set_background_color(arcade.color.WHITE)
+
+    arcade.start_render()
+
+    row = -1  # start at -1 so we can increment at 0
+    for (col, pil) in enumerate(pils):
+        if col % num_columns == 0:
+            row += 1
+        rect = basis_rect.in_grid(
+            row,
+            col % num_columns,
+            xpad=padding,
+            ypad=padding,
+        )
+        tex = arcade.Texture(
+            name=str(hash(pil.tobytes())),
+            image=pil,
+            hit_box_algorithm=None,
+        )
+        tex.draw_scaled(rect.center.x, rect.center.y)
+
+    arcade.finish_render()
+    arcade.run()
+    print("done")
