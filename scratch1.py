1bd9835234108aa5235510e0d76a63f0fef060a1
Med Mahmoud <medthehatta@gmail.com>
2024-09-28 13:45:32 -0400

    WIP

--- scratch1.py
+++ scratch1.py
@@ -26,38 +26,42 @@ def arcade_window(
     arcade.run()
 
 
-def render_module_collage(modules, num_columns=10, padding=2, margin=5):
+def render_module_collage(modules, num_columns=5, padding=2, margin=5):
     if not modules:
         raise ValueError("Must provide a nonempty list of modules")
     pils = [module_to_pil(mod) for mod in modules]
     pil_width = pils[0].width
     pil_height = pils[0].height
     basis_rect = (
-        RectLRBT.tlwh((0, 0), pil_width, pil_height)
+        RectLRBT.blwh((0, 0), pil_width, pil_height)
         .displace((margin, margin))
     )
 
-    width = num_columns * (pil_width + padding) - padding + 2*margin
     num_rows = len(pils) // num_columns
+    rows_then_columns = basis_rect.tiled(
+        rows=num_rows,
+        columns=num_columns,
+        xpad=padding,
+        ypad=padding,
+    ).as_iter_rows_columns()
+
+    width = num_columns * (pil_width + padding) - padding + 2*margin
     height = num_rows * (pil_height + padding) - padding + 2*margin
 
     with arcade_window(width, height, "Module collage"):
-        row = -1  # start at -1 so we can increment at 0
-        for (col, pil) in enumerate(pils):
-            if col % num_columns == 0:
-                row += 1
-            rect = basis_rect.in_grid(
-                row,
-                col % num_columns,
-                xpad=padding,
-                ypad=padding,
-            )
+        for (rect, pil) in zip(rows_then_columns, pils):
             tex = arcade.Texture(
                 name=str(hash(pil.tobytes())),
                 image=pil,
                 hit_box_algorithm=None,
             )
-            tex.draw_scaled(rect.center.x, rect.center.y)
+            scaled_rect = rect.scale_centered_pct(90)
+            tex.draw_sized(
+                scaled_rect.center.x,
+                scaled_rect.center.y,
+                scaled_rect.width,
+                scaled_rect.height,
+            )
 
     print("done")
 
@@ -98,7 +102,7 @@ def render_rect_sample():
         ]
     )
 
-    outer_rect = RectLRBT.blwh((0, 0), 800, 500)
+    outer_rect = RectLRBT.blwh((0, 0), 1024, 768)
     with arcade_window(outer_rect.width + 10, outer_rect.height + 10):
         artist.draw(inset_rect := outer_rect.scale_centered_pct(90))
         artist.draw(bottom_right_rect := inset_rect.subdivisions(2, 2).cell(row=0, column=1))
@@ -112,8 +116,8 @@ def render_rect_sample():
             lil_square :=
             lil_square_sub
             .at_center_with_width_height(
-                lil_square_sub.height,
-                lil_square_sub.height,
+                lil_square_sub.width,
+                lil_square_sub.width,
             )
             .scale_centered_pct(90)
         )
@@ -123,7 +127,7 @@ def render_rect_sample():
         )
 
 
-# seed = 20
-# rng = random.Random(seed)
-# render_module_collage(random_modules(20, rng))
-render_rect_sample()
+seed = 20
+rng = random.Random(seed)
+render_module_collage(random_modules(20, rng))
+# render_rect_sample()
